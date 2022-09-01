0875c83466207075cd0b356178493e9b9c310b71
nami <nami2528@naver.com>
2022-09-01 17:36:49 +0900

    storeList 생성

--- ECO4U/src/main/java/kr/spring/store/controller/StoreController.java
+++ ECO4U/src/main/java/kr/spring/store/controller/StoreController.java
@@ -1,5 +1,9 @@
 package kr.spring.store.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
@@ -13,18 +17,22 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import kr.spring.member.vo.MemberVO;
 import kr.spring.store.service.StoreService;
 import kr.spring.store.vo.StoreVO;
 import kr.spring.util.FileUtil;
+import kr.spring.util.PagingUtil;
 
 
 @Controller
 public class StoreController {
 	private static final Logger logger = 
 			LoggerFactory.getLogger(StoreController.class);
+	private int rowCount = 9;
+	private int pageCount = 10;
 	
 	@Autowired
 	private StoreService storeService;
@@ -41,10 +49,6 @@ public class StoreController {
 	public String eco4uForm() {
 		return "eco4u";
 	}
-	@GetMapping("/intro/store.do")
-	public String storeForm() {
-		return "store";
-	}
 
 	@GetMapping("/intro/storeRegister.do")
 	public String storeRegisterForm() {
@@ -52,9 +56,9 @@ public class StoreController {
 	}
 	
 	//회원가입 데이터 전송
-	@PostMapping("/intro/storeRegister.do")
-	public String submit(@Valid StoreVO storeVO, 
-					BindingResult result, Model model) {
+	@PostMapping("/intro/register.do")
+	public String submit(@Valid StoreVO storeVO,BindingResult result, 
+							HttpServletRequest request,Model model) {
 		logger.debug("<<가게 등록>> : " + storeVO);
 		
 		//유효성 체크 결과 오류가 있으면 폼 호출
@@ -62,25 +66,66 @@ public class StoreController {
 			return storeRegisterForm();
 		}
 		
-		ModelAndView mav = new ModelAndView();
-		
-		if(storeVO.getPhoto_name()==null) {
-			byte[] readbyte = FileUtil.getBytes(
-				request.getServletContext().getRealPath("/image_bundle/no_image.png"));
-			mav.addObject("imageFile", readbyte);
-			mav.addObject("filename", "no_image.png");
-			mav.setViewName("imageView");
-		}else {
-			memberVO = memberService.selectMember(user.getMem_num());
-			viewProfile(memberVO, request, mav);
-		}
 		//회원가입
 		storeService.insertStore(storeVO);
-		model.addAttribute("accessMsg", "가게 등록이 완료되었습니다.");
 		
+		//View에 표시할 메시지
+		model.addAttribute("accessMsg", "가게 등록이 완료되었습니다.");
+		model.addAttribute("url", request.getContextPath()+"/intro/storeRegister.do");
+				
 		return "common/notice";
 	}
 	
+	//============ 게시판 글 목록 =============//
+	@RequestMapping("/intro/store.do")
+	public ModelAndView process(
+			@RequestParam(value="pageNum",defaultValue="1") int currentPage) {
+		
+		Map<String,Object> map = new HashMap<String,Object>();
+		
+		
+		//글의 총개수(검색된 글의 개수
+		int count = storeService.selectRowCount(map);
+		
+		logger.debug("<<count>> : " + count);
+		
+		//페이지 처리
+		PagingUtil page = 
+				new PagingUtil(currentPage,count,
+								rowCount,pageCount,"store.do");
+		
+		List<StoreVO> list = null;
+		if(count > 0) {
+			map.put("start", page.getStartRow());
+			map.put("end", page.getEndRow());
+			
+			list = storeService.selectList(map);
+		}
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("store");
+		mav.addObject("count", count);
+		mav.addObject("list", list);
+		mav.addObject("page", page.getPage());
+		
+		return mav;
+	}
 	
+	//============ 이미지 출력 =============//
+	@RequestMapping("/intro/imageView.do")
+	public ModelAndView viewImage(@RequestParam int s_num) {
+		System.out.println("s-num : " + s_num);
+		StoreVO store = storeService.selectStore(s_num);
+		
+		ModelAndView mav = new ModelAndView();
+		//뷰 이름
+		mav.setViewName("imageView");
+	
+		mav.addObject("imageFile", store.getPhoto());
+		mav.addObject("filename", store.getPhoto_name());
+	
+		return mav;
+	}
+
 	
 }

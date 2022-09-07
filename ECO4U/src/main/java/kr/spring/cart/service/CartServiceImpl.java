c15498a6a54739eec564865ce16692a46a2d07ad
soooobbbin <soobinlee716@gmail.com>
2022-09-07 15:09:37 +0900

    장바구니 결제 버튼 추가

---
+++ ECO4U/src/main/java/kr/spring/cart/service/CartServiceImpl.java
@@ -0,0 +1,47 @@
+package kr.spring.cart.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import kr.spring.cart.dao.CartMapper;
+import kr.spring.cart.vo.CartVO;
+
+@Service
+@Transactional
+public class CartServiceImpl implements CartService{
+	
+	@Autowired
+	private CartMapper cartMapper;
+
+	@Override
+	public List<CartVO> selectList(Map<String, Object> map) {
+		return cartMapper.selectList(map);
+	}
+
+	@Override
+	public int selectRowCount(Map<String, Object> map) {
+		return cartMapper.selectRowCount(map);
+	}
+
+	@Override
+	public CartVO selectCart(Integer cart_num) {
+		return cartMapper.selectCart(cart_num);
+	}
+
+	@Override
+	public void updateCart(Integer cart_num) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteCart(Integer cart_num) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

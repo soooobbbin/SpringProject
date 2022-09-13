fea50e97b80d3e4f9ee05a0b2fe967f6101e4180
YoonHY <72802183+YoonHY@users.noreply.github.com>
2022-09-13 13:44:32 +0900

    회원조회_페이지#1

--- ECO4U/src/main/java/kr/spring/member/dao/MemberMapper.java
+++ ECO4U/src/main/java/kr/spring/member/dao/MemberMapper.java
@@ -67,12 +67,8 @@ public interface MemberMapper {
 	public void deletezipcode(Integer mem_num);
 	
 	//회원관리-관리자
-	/*
-	 * public List<MemberVO> selectList(Map<String,Object>map); public int
-	 * selectRowCount(Map<String,Object>map);
-	 * 
-	 * @Update("UPDATE member SET auth=#{auth} WHERE mem_num=#{mem_num}") public
-	 * void updateByAdmin(MemberVO memberVO);
-	 */
+	public List<MemberVO> selectList(Map<String,Object>map);
+	public int selectRowCount(Map<String,Object>map);
+	
 	
 }
